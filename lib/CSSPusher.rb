c200e1125bb99c12165be15e1b425fcdd56ff817
The JSilver <jsilverman2@gmail.com>
2012-04-04 04:47:22 -0700

    add pusher

---
+++ lib/CSSPusher.rb
@@ -0,0 +1,30 @@
+require 'css_parser'
+include CssParser
+
+class CSSPusher
+
+  def initialize(css, dir, amt)
+
+    @parser = CssParser::Parser.new
+    @parser.load_uri!(css)
+
+    @parser.each_rule_set do |rule_set|
+      if dir == 'right'
+      rule_set['left'] = "#{rule_set['left'].to_i + amt}px;"
+      elsif dir == 'left'
+        rule_set['left'] = "#{rule_set['left'].to_i - amt}px;"
+      elsif dir == 'up'
+        rule_set['top'] = "#{rule_set['top'].to_i - amt}px;"
+      elsif dir == 'down'
+        rule_set['top'] = "#{rule_set['top'].to_i + amt}px;"
+      end
+    end
+
+  end
+
+  def out
+    @parser.to_s
+  end
+
+
+end
